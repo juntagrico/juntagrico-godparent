7a9ac6123495f5cd1bf32cb51c32db668b628416
David Glenck <dgl@climeworks.com>
2022-02-24 21:20:58 +0100

    fix version in setup

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name=juntagrico_godparent.name,
-    version=juntagrico_godparent.name,
+    version=juntagrico_godparent.version,
     packages=find_packages(),
     include_package_data=True,
     license='LPGLv3',  # example license
