1650fa2392cd91239e96d81288ec2944b8522d76
pascalfree <pascal.free@ihdg.ch>
2024-12-29 16:54:50 +0100

    use default values

--- juntagrico_godparent/static/jgo/js/initUnmatchable.js
+++ juntagrico_godparent/static/jgo/js/initUnmatchable.js
@@ -1,15 +1,5 @@
 $(function(){
     $("#filter-table-godparents").DataTable({
-        "paging": false,
-        "info": false,
         "ordering": true,
-        "search": {
-            "regex": true,
-            "smart": false
-        },
-        "language": {
-            "search": search_field,
-            searchBuilder: sb_lang
-        },
     })
 })
